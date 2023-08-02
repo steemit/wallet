5a51537b829842288a138510da93e24bd42278c9
symbionts-dev-ops <utopian.davinci@gmail.com>
2023-08-02 16:28:02 +0000

    Avoid negative values in the step parameter

--- src/app/components/elements/OutgoingDelegationsTables/Pagination.jsx
+++ src/app/components/elements/OutgoingDelegationsTables/Pagination.jsx
@@ -2,12 +2,13 @@
 /* eslint react/prop-types: 0 */
 import React, { Component } from 'react';
 import shouldComponentUpdate from 'app/utils/shouldComponentUpdate';
-import { LEFT_PAGE, RIGHT_PAGE } from './constants'
+import { LEFT_PAGE, RIGHT_PAGE } from './constants';
 /**
  * Helper method for creating a range of numbers
  * range(1, 5) => [1, 2, 3, 4, 5]
  */
 const range = (from, to, step = 1) => {
+    step = step < 1 ? 1 : step;
     let i = from;
     const rng = [];
     while (i <= to) {
@@ -15,19 +16,24 @@ const range = (from, to, step = 1) => {
         i += step;
     }
     return rng;
-}
+};
 
 class Pagination extends Component {
     constructor(props) {
         super(props);
         this.shouldComponentUpdate = shouldComponentUpdate(this, 'Pagination');
-        const { totalRecords = null, pageLimit = 100, pageNeighbours = 0 } = props;
+        const {
+            totalRecords = null,
+            pageLimit = 100,
+            pageNeighbours = 0,
+        } = props;
         this.pageLimit = typeof pageLimit === 'number' ? pageLimit : 100;
         this.totalRecords = typeof totalRecords === 'number' ? totalRecords : 0;
         // pageNeighbours can be: 0, 1 or 2
-        this.pageNeighbours = typeof pageNeighbours === 'number'
-            ? Math.max(0, Math.min(pageNeighbours, 2))
-            : 0;
+        this.pageNeighbours =
+            typeof pageNeighbours === 'number'
+                ? Math.max(0, Math.min(pageNeighbours, 2))
+                : 0;
         this.totalPages = Math.ceil(this.totalRecords / this.pageLimit);
         this.state = { currentPage: 1 };
     }
@@ -44,28 +50,25 @@ class Pagination extends Component {
         };
 
         this.setState({ currentPage }, () => onPageChanged(paginationData));
-    }
+    };
 
     handleClick = page => evt => {
         evt.preventDefault();
         this.gotoPage(page);
-    }
+    };
 
     handleMoveLeft = evt => {
         evt.preventDefault();
-        this.gotoPage(this.state.currentPage - (this.pageNeighbours * 2) - 1);
-    }
+        this.gotoPage(this.state.currentPage - this.pageNeighbours * 2 - 1);
+    };
 
     handleMoveRight = evt => {
         evt.preventDefault();
-        this.gotoPage(this.state.currentPage + (this.pageNeighbours * 2) + 1);
-    }
+        this.gotoPage(this.state.currentPage + this.pageNeighbours * 2 + 1);
+    };
 
     render() {
-        const {
-            totalRecords,
-            pageLimit
-        } = this.props
+        const { totalRecords, pageLimit } = this.props;
         /**
          * Let's say we have 10 pages and we set pageNeighbours to 2
          * Given that the current page is 6
@@ -86,11 +89,14 @@ class Pagination extends Component {
              * totalNumbers: the total page numbers to show on the control
              * totalBlocks: totalNumbers + 2 to cover for the left(<) and right(>) controls
              */
-            const totalNumbers = (this.pageNeighbours * 2) + 3;
+            const totalNumbers = this.pageNeighbours * 2 + 3;
             const totalBlocks = totalNumbers + 2;
             if (totalPages > totalBlocks) {
                 const startPage = Math.max(2, currentPage - pageNeighbours);
-                const endPage = Math.min(totalPages - 1, currentPage + pageNeighbours);
+                const endPage = Math.min(
+                    totalPages - 1,
+                    currentPage + pageNeighbours
+                );
                 let pages = range(startPage, endPage);
                 /**
                  * hasLeftSpill: has hidden pages to the left
@@ -98,23 +104,29 @@ class Pagination extends Component {
                  * spillOffset: number of hidden pages either to the left or to the right
                  */
                 const hasLeftSpill = startPage > 2;
-                const hasRightSpill = (totalPages - endPage) > 1;
+                const hasRightSpill = totalPages - endPage > 1;
                 const spillOffset = totalNumbers - (pages.length + 1);
                 switch (true) {
                     // handle: (1) < {5 6} [7] {8 9} (10)
-                    case (hasLeftSpill && !hasRightSpill): {
-                        const extraPages = range(startPage - spillOffset, startPage - 1);
+                    case hasLeftSpill && !hasRightSpill: {
+                        const extraPages = range(
+                            startPage - spillOffset,
+                            startPage - 1
+                        );
                         pages = [LEFT_PAGE, ...extraPages, ...pages];
                         break;
                     }
                     // handle: (1) {2 3} [4] {5 6} > (10)
-                    case (!hasLeftSpill && hasRightSpill): {
-                        const extraPages = range(endPage + 1, endPage + spillOffset);
+                    case !hasLeftSpill && hasRightSpill: {
+                        const extraPages = range(
+                            endPage + 1,
+                            endPage + spillOffset
+                        );
                         pages = [...pages, ...extraPages, RIGHT_PAGE];
                         break;
                     }
                     // handle: (1) < {4 5} [6] {7 8} > (10)
-                    case (hasLeftSpill && hasRightSpill):
+                    case hasLeftSpill && hasRightSpill:
                     default: {
                         pages = [LEFT_PAGE, ...pages, RIGHT_PAGE];
                         break;
@@ -123,7 +135,7 @@ class Pagination extends Component {
                 return [1, ...pages, totalPages];
             }
             return range(1, totalPages);
-        }
+        };
         if (!totalRecords || this.totalPages === 1) return null;
 
         const { currentPage } = this.state;
@@ -133,27 +145,54 @@ class Pagination extends Component {
             <nav className="navPagination">
                 <ul className="Pagination">
                     {pages.map((page, index) => {
+                        if (page === LEFT_PAGE)
+                            return (
+                                <li
+                                    key={`${LEFT_PAGE}-${index}`}
+                                    className="page-item"
+                                >
+                                    <button
+                                        className="page-link text--capitalize"
+                                        aria-label="Previous"
+                                        onClick={this.handleMoveLeft}
+                                    >
+                                        <span aria-hidden="true">&laquo;</span>
+                                        <span className="sr-only">
+                                            Previous
+                                        </span>
+                                    </button>
+                                </li>
+                            );
 
-                        if (page === LEFT_PAGE) return (
-                            <li key={`${LEFT_PAGE}-${index}`} className="page-item">
-                                <button className="page-link text--capitalize" aria-label="Previous" onClick={this.handleMoveLeft}>
-                                    <span aria-hidden="true">&laquo;</span>
-                                    <span className="sr-only">Previous</span>
-                                </button>
-                            </li>
-                        );
-
-                        if (page === RIGHT_PAGE) return (
-                            <li key={`${RIGHT_PAGE}-${index}`} className="page-item">
-                                <button className="page-link text--capitalize" aria-label="Next" onClick={this.handleMoveRight}>
-                                    <span aria-hidden="true">&raquo;</span>
-                                    <span className="sr-only">Next</span>
-                                </button>
-                            </li>
-                        );
+                        if (page === RIGHT_PAGE)
+                            return (
+                                <li
+                                    key={`${RIGHT_PAGE}-${index}`}
+                                    className="page-item"
+                                >
+                                    <button
+                                        className="page-link text--capitalize"
+                                        aria-label="Next"
+                                        onClick={this.handleMoveRight}
+                                    >
+                                        <span aria-hidden="true">&raquo;</span>
+                                        <span className="sr-only">Next</span>
+                                    </button>
+                                </li>
+                            );
                         return (
-                            <li key={`pagenumber-${index}`} className={`page-item${currentPage === page ? ' active' : ''}`}>
-                                <button className="page-link" onClick={this.handleClick(page)}>{page}</button>
+                            <li
+                                key={`pagenumber-${index}`}
+                                className={`page-item${
+                                    currentPage === page ? ' active' : ''
+                                }`}
+                            >
+                                <button
+                                    className="page-link"
+                                    onClick={this.handleClick(page)}
+                                >
+                                    {page}
+                                </button>
                             </li>
                         );
                     })}
