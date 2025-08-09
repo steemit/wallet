1f51b9eeb31bfa8c5a6e4eebf6ca3b35502cd090
Blaze Apps <214065908+blazeapps007@users.noreply.github.com>
2025-08-09 15:16:11 +0545

    Update UserWallet.jsx

--- src/app/components/modules/UserWallet.jsx
+++ src/app/components/modules/UserWallet.jsx
@@ -619,7 +619,7 @@ class UserWallet extends React.Component {
         let advancedRoutesNotification = null;
         if (isMyAccount && withdraw_routes && withdraw_routes.size > 0) {
             const message =
-                'Additional Routes Are configured for Receiving Vesting Payments, Please re-confirm on Advanced Routes Options.';
+                'Withdrawal routes are configured to receive vesting payments. Please reconfirm in the Advanced Routes options.';
 
             advancedRoutesNotification = (
                 <div className="UserWallet__balance row">
